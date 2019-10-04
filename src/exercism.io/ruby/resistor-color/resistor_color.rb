bff730f67302ee10820093a93c935147e6456a2f
Tiago Ferreira <tiferreira12@gmail.com>
2019-10-04 13:13:59 -0300

    Exercism.io - ResistorColor

---
+++ src/exercism.io/ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ResistorColor
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+  def self.color_code(color)
+    COLORS.index(color)
+  end
+end
+
+puts ResistorColor.color_code 'red'
