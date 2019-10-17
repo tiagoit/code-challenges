37b43514f57128f3d0ce16d630892c6f4f92b009
Tiago Ferreira <tiferreira12@gmail.com>
2019-10-17 11:16:29 -0300

    HackerRank - 25 - Divisible Sum Pairs

---
+++ src/hackerrank/25-divisible-sum-pairs/divisible_sum_pairs.rb
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/divisible-sum-pairs
+# HackerRank - 25 - Divisible Sum Pairs - 20191017
+
+def divisible_sum_pairs n, k, ar
+  pairs = 0
+  n.times do |i|
+    ((i + 1)...n).each do |j|
+      pairs += 1 if ((ar[i] + ar[j]) % k).zero?
+      ar[i]
+    end
+  end
+  pairs
+end
+
+def solve
+  puts(divisible_sum_pairs(6, 3, [1, 3, 2, 6, 1, 2]))
+end
--- src/hackerrank/main.rb
+++ src/hackerrank/main.rb
@@ -3,6 +3,7 @@
 
 # require_relative './22-palindrome-index/palindrome_index'
 # require_relative './23-game-of-thrones/game_of_thrones'
-require_relative './24-birthday-cake-candles/birthday_cake_candles'
+# require_relative './24-birthday-cake-candles/birthday_cake_candles'
+require_relative './25-divisible-sum-pairs/divisible_sum_pairs'
 
 solve
