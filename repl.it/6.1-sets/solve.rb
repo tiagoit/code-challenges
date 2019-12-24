5946ef0d164bc9d71960c157394e924563e17635
Tiago Ferreira <tiferreira12@gmail.com>
2019-12-24 00:56:32 -0300

    Repl.it - 6.1 - Sets

---
+++ repl.it/6.1-sets/solve.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+def find_duplicates(array)
+  s = Set[]
+  duplicates = []
+  array.each do |item|
+    if(s.include? item)
+      duplicates.push(item)
+    else
+      s.add(item)
+    end
+  end
+  duplicates
+end
+
+p find_duplicates([1, 2, 3, 1, 5, 6, 7, 8, 5, 2])
+# => [1, 5, 2]
+
+p find_duplicates([3, 501, 17, 23, -43, 67, 5, 888, -402, 235, 77, 99, 311, 1, -43])
+# => [-43]
